3016b9dfc6e550af1509c0237b71f855d666a5f1
Jairo Franchi <jairofranchi@MacBook-Pro-de-Jairo.local>
2020-04-02 21:50:24 -0300

    End of Section 5

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,7 +1,8 @@
-using System.Text;
+using System.Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Dating.API.Data;
 using DatingApp.API.Data;
@@ -16,50 +17,75 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DatingApp.API.Helpers;
 
-namespace Dating.API {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
+namespace Dating.API
+{
+    public class Startup
+    {
+        public Startup (IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
-            services.AddDbContext<DataContext> 
+        public void ConfigureServices (IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>
                 (x => x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
             services.AddControllers ();
-            services.AddCors();
-            services.AddScoped<IAuthRepository, AuthRepository>();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            services.AddCors ();
+            services.AddScoped<IAuthRepository, AuthRepository> ();
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer (options => options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
-                        .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
+                        IssuerSigningKey = new SymmetricSecurityKey (Encoding.ASCII
+                            .GetBytes (Configuration.GetSection ("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
                 });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment ()) {
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment ())
+            {
                 app.UseDeveloperExceptionPage ();
+            }
+            else 
+            {
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if(error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
 
             //app.UseHttpsRedirection();
 
             app.UseRouting ();
 
-            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseCors (builder => builder.AllowAnyOrigin ().AllowAnyMethod ().AllowAnyHeader ());
 
-            app.UseAuthentication();
-
+            app.UseAuthentication ();
+    
             app.UseAuthorization ();
 
-            app.UseEndpoints (endpoints => {
+            app.UseEndpoints (endpoints =>
+            {
                 endpoints.MapControllers ();
             });
         }
